1d13e720478f9a321e63ff40fe86cf1bd4e46ec4
Sourabh Agarwal <sourabhagarwal32@gmail.com>
2022-07-18 03:00:31 +0530

    first commit

---
+++ precache-manifest.29f5e672d0f5e91e95a8f983800de2b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91169eb8123b35ec4e09",
+    "url": "/static/css/main.9d8556d6.chunk.css"
+  },
+  {
+    "revision": "91169eb8123b35ec4e09",
+    "url": "/static/js/main.91169eb8.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "ff6bbdc6cfbfc76612c93f901089a01e",
+    "url": "/index.html"
+  }
+];
